0bd4526a24156a6c22ca6f4e8bf4a3c1d6913ff3
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-12-09 18:27:25 +0000

    update to include score of zero

--- app.js
+++ app.js
@@ -11,11 +11,9 @@ const computeScore = () => {
 };
 
 const appendInfo = () => {
-  if (curbScore > 0) {
-    const mortality = `<p>Mortality: ${curb65[curbScore - 1].mortality}%</p>`;
-    const recommendation = `<p>Recommendation: ${curb65[curbScore - 1].recommendation}</p>`;
-    infoBox.insertAdjacentHTML('beforeend', `${mortality} ${recommendation}`);
-  }
+  const mortality = `<p>Mortality: ${curb65[curbScore].mortality}%</p>`;
+  const recommendation = `<p>Recommendation: ${curb65[curbScore].recommendation}</p>`;
+  infoBox.insertAdjacentHTML('beforeend', `${mortality} ${recommendation}`);
 };
 
 // Maximum score of one for hypotension
@@ -42,9 +40,10 @@ checkboxes.forEach((checkbox) => {
 });
 
 const curb65 = [
-  { score: 1, mortality: 0.6, recommendation: 'Low risk; consider outpatient treatment' },
-  { score: 2, mortality: 2.7, recommendation: 'Low risk; consider outpatient treatment' },
-  { score: 3, mortality: 6.8, recommendation: 'Short inpatient hospitalization or closely supervised outpatient treatment' },
+  { score: 0, mortality: 0.6, recommendation: 'Low risk; consider outpatient treatment' },
+  { score: 1, mortality: 2.7, recommendation: 'Low risk; consider outpatient treatment' },
+  { score: 2, mortality: 6.8, recommendation: 'Short inpatient hospitalization or closely supervised outpatient treatment' },
+  { score: 3, mortality: 14, recommendation: 'Severe pneumonia; hospitalize and consider admitting to intensive care' },
   { score: 4, mortality: 27.8, recommendation: 'Severe pneumonia; hospitalize and consider admitting to intensive care' },
   { score: 5, mortality: 27.8, recommendation: 'Severe pneumonia; hospitalize and consider admitting to intensive care' },
 ];
