b9c2a57cc6fe42df4d04ac61f5bf8df3e253888c
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-12-09 17:15:01 +0000

    max score of one for hypotension

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const checkboxes = document.querySelectorAll('.checkbox');
 const scoreBox = document.querySelector('#score');
+const systolicBP = document.querySelector('#sbp');
+const diastolicBP = document.querySelector('#dbp');
 let curbScore = 0;
 
+// Maximum score of one for hypotension
+const checkHypotension = () => {
+  diastolicBP.value = systolicBP.checked ? 0 : 1;
+  systolicBP.value = diastolicBP.checked ? 0 : 1;
+};
+
+const incrementScore = (checkbox) => {
+  checkHypotension();
+  if (checkbox.checked) {
+    curbScore += parseInt(checkbox.value, 10);
+    scoreBox.innerText = curbScore;
+  } else {
+    curbScore -= parseInt(checkbox.value, 10);
+    scoreBox.innerText = curbScore;
+  }
+};
+
 checkboxes.forEach((checkbox) => {
-  checkbox.addEventListener('change', () => {
-    if (checkbox.checked) {
-      curbScore += 1;
-      scoreBox.innerText = curbScore;
-    } else {
-      curbScore -= 1;
-      scoreBox.innerText = curbScore;
-    }
-  });
+  checkbox.addEventListener('change', () => { incrementScore(checkbox); });
 });
