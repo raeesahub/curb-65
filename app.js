9bdeb9aec8d9f18558c067cb67a180b6ce2d3dc9
Raeesa Qureshi <qureshi.raeesa@gmail.com>
2021-12-09 22:23:06 +0000

    hide info box by default

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const checkboxes = document.querySelectorAll('.checkbox');
 const scoreBox = document.querySelector('#score');
 const infoBox = document.querySelector('#info');
+const recommendationBox = document.querySelector('.recommendation');
 const systolicBP = document.querySelector('#sbp');
 const diastolicBP = document.querySelector('#dbp');
+
 let curbScore = 0;
 
 const computeScore = () => {
@@ -11,6 +13,7 @@ const computeScore = () => {
 };
 
 const appendInfo = () => {
+  recommendationBox.style.display = 'block';
   const mortality = `<p>Mortality: ${curb65[curbScore].mortality}%</p>`;
   const recommendation = `<p>${curb65[curbScore].recommendation}</p>`;
   infoBox.insertAdjacentHTML('beforeend', `${mortality} ${recommendation}`);
@@ -42,8 +45,8 @@ checkboxes.forEach((checkbox) => {
 const curb65 = [
   { score: 0, mortality: 0.6, recommendation: 'Low risk; consider outpatient treatment' },
   { score: 1, mortality: 2.7, recommendation: 'Low risk; consider outpatient treatment' },
-  { score: 2, mortality: 6.8, recommendation: 'Short inpatient hospitalization or closely supervised outpatient treatment' },
-  { score: 3, mortality: 14, recommendation: 'Severe pneumonia; hospitalize and consider admitting to intensive care' },
-  { score: 4, mortality: 27.8, recommendation: 'Severe pneumonia; hospitalize and consider admitting to intensive care' },
-  { score: 5, mortality: 27.8, recommendation: 'Severe pneumonia; hospitalize and consider admitting to intensive care' },
+  { score: 2, mortality: 6.8, recommendation: 'Short inpatient hospitalisation or closely supervised outpatient treatment' },
+  { score: 3, mortality: 14, recommendation: 'Severe pneumonia; hospitalise and consider admitting to intensive care' },
+  { score: 4, mortality: 27.8, recommendation: 'Severe pneumonia; hospitalise and consider admitting to intensive care' },
+  { score: 5, mortality: 27.8, recommendation: 'Severe pneumonia; hospitalise and consider admitting to intensive care' },
 ];
